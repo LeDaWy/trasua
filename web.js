5471193b96b7eeaf14f7d66eb485c4d7139d2b04
LeDaWy <legiahuy260710@gmail.com>
2025-05-28 21:30:21 +0700

    Update web.js

--- web.js
+++ web.js
@@ -146,10 +146,3 @@ function saveAddress() {
 }
 
 //-----------------Tài Khoản
-const user = JSON.parse(localStorage.getItem("loggedInUser"));
-      if (user) {
-        document.getElementById("user-number").textContent =
-          user.number || "Chưa có";
-        document.getElementById("user-address").textContent =
-          user.address || "Chưa nhập";
-      }
